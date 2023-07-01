180f29a3063db54ed956991a663188cb0954b450
Ramon Ribeiro <ramon.procisa@gmail.com>
2023-07-01 19:32:38 -0300

    Finished program

--- src/model/services/RentalService.java
+++ src/model/services/RentalService.java
@@ -10,12 +10,12 @@ public class RentalService {
     private Double pricePerHour;
     private Double pricePerDay;
 
-    private BrazilTaxService taxService;
+    private TaxService taxService;
 
     //Não ha necessiadade de colocar o construtor vazio, pois assim obriga o usuário a colocar
     // os dados abaixo, preço por hora, preço por dia e taxa
 
-    public RentalService(Double pricePerHour, Double pricePerDay, BrazilTaxService taxService) {
+    public RentalService(Double pricePerHour, Double pricePerDay, TaxService taxService) {
         this.pricePerHour = pricePerHour;
         this.pricePerDay = pricePerDay;
         this.taxService = taxService;
